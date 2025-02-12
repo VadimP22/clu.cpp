fc7f37014d0e076ba79cb47b98039b0cb3e9345a
Vadim Petrov <coolstudiorus@gmail.com>
2025-02-12 22:04:38 +0300

    Additions/changes:   * LuaWriter class. It will generate Lua code     and hold necessary context.   * Printing module in stdout was moved into     separate function.

--- clu.cpp
+++ clu.cpp
@@ -1,33 +1,41 @@
 #include "clu.h"
-#include "llvm/Support/raw_ostream.h"
 
-int main(int argc, const char ** argv) {
-    using namespace llvm;
-
-    if (argc == 1) {
-        errs() << "Path to file expected\n";
-        return 1;
-    }
+void clu::LuaWriter::debugPrintModule(llvm::raw_ostream& out, llvm::Module& module) {
+    auto& functions = module.getFunctionList();
     
-    LLVMContext context{};
-    SMDiagnostic error{};
+    llvm::outs() << "Module: " << module.getName() << "\n";
     
-    std::unique_ptr<Module> module = parseIRFile(argv[1], error, context);
-    if (!module) {
-        error.print(argv[0], errs());
-    }
-
-    auto& functions = module->getFunctionList();
     for (auto itt = functions.begin(); itt != functions.end(); ++itt) {
         auto& current_function = *itt;
-        outs() << "Function: " << current_function.getName() << "\n";
+        llvm::outs() << "    Function: " << current_function.getName() << "\n";
         
         auto& block = current_function.getEntryBlock();
         for (auto itt = block.begin(); itt != block.end(); ++itt) {
             auto& current_instruction = *itt;
-            outs() << "    Instruction: " << current_instruction.getOpcodeName() << "\n";
+            llvm::outs() << "        Instruction: " << current_instruction.getOpcodeName() << "\n";
+            llvm::outs() << "            Text: " << current_instruction << "\n";
         }
     }
+}
 
+int main(int argc, const char ** argv) {
+    // using namespace llvm;
+
+    if (argc == 1) {
+        llvm::errs() << "Path to file expected\n";
+        return 1;
+    }
+    
+    llvm::LLVMContext context{};
+    llvm::SMDiagnostic error{};
+    clu::LuaWriter lua_writer{};
+    
+    std::unique_ptr<llvm::Module> module = parseIRFile(argv[1], error, context);
+    if (!module) {
+        error.print(argv[0], llvm::errs());
+    }
+
+    lua_writer.debugPrintModule(llvm::outs(), *module);    
+    
     return 0;
 }
--- clu.h
+++ clu.h
@@ -1,9 +1,22 @@
+#include "llvm/Support/raw_ostream.h"
 #include <llvm/IRReader/IRReader.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Support/SourceMgr.h>
-#include "llvm/Support/raw_ostream.h"
 
 namespace clu {
     
-}
+class LuaWriter {
+public:
+    
+    void writeModule(llvm::raw_ostream&, llvm::Module&);
+    void debugPrintModule(llvm::raw_ostream&, llvm::Module&);
+
+private:
+    void writeFunction(std::ostream&, llvm::Function&);
+    void writeBasicBlock(std::ostream&, llvm::BasicBlock&);
+
+    std::string out_file_path_;
+};
+    
+} // namespace clu
